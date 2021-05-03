9462a593d92beaae6ccddc9dfe9bb9a28f8c0f6e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-05-03 23:12:55 +0900

    Update project_urls

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     packages=setuptools.find_packages(exclude=["tests*"]),
     project_urls={
-        "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
     },
