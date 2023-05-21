5ae6a490e095138148bd4edf2c553375ffb53ed7
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-05-21 18:44:26 +0900

    Remove an unused class

--- cli_project_name/_logger.py
+++ cli_project_name/_logger.py
@@ -1,6 +1,8 @@
 import enum
 import sys
 
+from loguru import logger
+
 from ._const import MODULE_NAME
 
 
@@ -8,57 +10,6 @@ class LogLevel(enum.Enum):
     DEBUG = "DEBUG"
     INFO = "INFO"
     QUIET = "QUIET"
-
-
-class NullLogger:
-    level_name = None
-
-    def remove(self, handler_id=None):  # pragma: no cover
-        pass
-
-    def add(self, sink, **kwargs):  # pragma: no cover
-        pass
-
-    def disable(self, name):  # pragma: no cover
-        pass
-
-    def enable(self, name):  # pragma: no cover
-        pass
-
-    def critical(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def debug(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def error(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def exception(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def info(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def log(self, __level, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def success(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def trace(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-    def warning(self, __message, *args, **kwargs):  # pragma: no cover
-        pass
-
-
-try:
-    from loguru import logger
-
-    logger.disable(MODULE_NAME)
-except ImportError:
-    logger = NullLogger()  # type: ignore
 
 
 def set_logger(is_enable: bool, propagation_depth: int = 1) -> None:
